f986bd33a5f079df4bd5a0da6a55f6d256ea63e3
Ziqiang Shen <bxandf@uw.edu>
2015-05-10 08:49:42 -0700

    timeline done

---
+++ admin/datajs/search.js
@@ -0,0 +1,61 @@
+$("#submit").click(function(e){
+	
+	$("#submit").attr("disabled", true);
+	var _url = "http://52.24.102.160:8080/getdeals"
+	 $.ajax({
+        type : 'GET',
+        url : _url,           
+        data: $("#input"),
+        success: function (msg) {
+			var results = msg.results;
+			var left = true;
+			var res = "";
+			for (var i=0; i<results.length; i++) {
+				var content;
+				if (left) {
+					content = 
+							"<div class='timeline-item alt'>"+
+                      		"<div class='timeline-desk'>"+
+                          	"<div class='panel'>"+
+                            "<div class='panel-body'>"+
+                            "<span class='timeline-icon red'>"+
+                            "<i class='fa fa-check-square-o'></i>"+
+							"</span><h1>";
+					content+= results[i].DealDate;
+					content+= "</h1><p><strong>Deal Title:</strong> ";
+					content+= results[i].DealTitle;
+					content+= "</p><p><strong>Associate:</strong> ";
+					content+= (results[i].FirstName + ' ' + results[i].LastName);
+					content+= "</p><p><strong>Contact:</strong> ";
+					content+= results[i].EmailAddress;
+					content+= "</p><p><strong>Revenue:</strong> ";
+					content+= results[i].Revenue;
+					content+= "</p></div></div></div></div>";
+
+				} else {
+					content =
+					"<div class='timeline-item'>"+
+                    "<div class='timeline-desk'>"+
+                    "<div class='panel'>"+
+                    "<div class='panel-body'>"+
+                    "<span class='timeline-icon red'>"+
+                    "<i class='fa fa-check-square-o'></i>"+
+					"</span><h1>";
+					content+= results[i].DealDate;
+					content+= "</h1><p><strong>Deal Title:</strong> ";
+					content+= results[i].DealTitle;
+					content+= "</p><p><strong>Associate:</strong> ";
+					content+= (results[i].FirstName + ' ' + results[i].LastName);
+					content+= "</p><p><strong>Contact:</strong> ";
+					content+= results[i].EmailAddress;
+					content+= "</p><p><strong>Revenue:</strong> ";
+					content+= results[i].Revenue;
+					content+= "</p></div></div></div></div>";
+				} 
+				left = !left;
+				res += content;
+			}
+			$("#result").html(res);
+		}
+	 });
+});
